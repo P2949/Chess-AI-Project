0391285f5d3f6d1b49af7b629802cf7ddbda720d
P2949 <torbypedro@gmail.com>
2026-03-15 15:57:40 +0000

    just tried adding some more evaluation data to stop some stupid forks

--- team_goraieb.py
+++ team_goraieb.py
@@ -88,6 +88,132 @@ PST_KING_EG = [
     -50,-30,-30,-30,-30,-30,-30,-50,
 ]
 
+PST_PAWN_MG = [
+      0,   0,   0,   0,   0,   0,   0,   0,
+     98, 134,  61,  95,  68, 126,  34, -11,
+     -6,   7,  26,  31,  62,  22,  -8, -17,
+    -14,  13,   6,  21,  23,  12,  17, -23,
+    -27,  -2,  -5,  12,  17,   6,  10, -25,
+    -26,  -4,  -4, -10,   3,   3,  33, -12,
+    -35,  -1, -20, -23, -15,  24,  38, -22,
+      0,   0,   0,   0,   0,   0,   0,   0,
+]
+PST_PAWN_EG = [
+      0,   0,   0,   0,   0,   0,   0,   0,
+    178, 173, 158, 134, 147, 132, 165, 187,
+     94, 100,  85,  67,  56,  53,  82,  84,
+     32,  24,  13,   5,  -2,   4,  17,  17,
+     13,   9,  -3,  -7,  -7,  -8,   3,  -1,
+      4,   7,  -6,   1,   0,  -5,  -1,  -8,
+     13,   8,   8,  10,  13,   0,   2,  -7,
+      0,   0,   0,   0,   0,   0,   0,   0,
+]
+
+PST_KNIGHT_MG = [
+    -167, -89, -34, -49,  61, -97, -15, -107,
+     -73, -41,  72,  36,  23,  62,   7,  -17,
+     -47,  60,  37,  65,  84, 129,  73,   44,
+      -9,  17,  19,  53,  37,  69,  18,   22,
+     -13,   4,  16,  13,  28,  19,  21,   -8,
+     -23,  -9,  12,  10,  19,  17,  25,  -16,
+     -29, -53, -12,  -3,  -1,  18, -14,  -19,
+    -105, -21, -58, -33, -17, -28, -19,  -23,
+]
+PST_KNIGHT_EG = [
+    -58, -38, -13, -28, -31, -27, -63, -99,
+    -25,  -8, -25,  -2,  -9, -25, -24, -52,
+    -24, -20,  10,   9,  -1,  -9, -19, -41,
+    -17,   3,  22,  22,  22,  11,   8, -18,
+    -18,  -6,  16,  25,  16,  17,   4, -18,
+    -23,  -3,  -1,  15,  10,  -3, -20, -22,
+    -42, -20, -10,  -5,  -2, -20, -23, -44,
+    -29, -51, -23, -15, -22, -18, -50, -64,
+]
+
+PST_BISHOP_MG = [
+    -29,   4, -82, -37, -25, -42,   7,  -8,
+    -26,  16, -18, -13,  30,  59,  18, -47,
+    -16,  37,  43,  40,  35,  50,  37,  -2,
+     -4,   5,  19,  50,  37,  37,   7,  -2,
+     -6,  13,  13,  26,  34,  12,  10,   4,
+      0,  15,  15,  15,  14,  27,  18,  10,
+      4,  15,  16,   0,   7,  21,  33,   1,
+    -33,  -3, -14, -21, -13, -12, -39, -21,
+]
+PST_BISHOP_EG = [
+    -14, -21, -11,  -8,  -7,  -9, -17, -24,
+     -8,  -4,   7, -12,  -3, -13,  -4, -14,
+      2,  -8,   0,  -1,  -2,   6,   0,   4,
+     -3,   9,  12,   9,  14,  10,   3,   2,
+     -6,   3,  13,  19,   7,  10,  -3,  -9,
+    -12,  -3,   8,  10,  13,   3,  -7, -15,
+    -14, -18,  -7,  -1,   4,  -9, -15, -27,
+    -23,  -9, -23,  -5,  -9, -16,  -5, -17,
+]
+
+PST_ROOK_MG = [
+     32,  42,  32,  51,  63,   9,  31,  43,
+     27,  32,  58,  62,  80,  67,  26,  44,
+     -5,  19,  26,  36,  17,  45,  61,  16,
+    -24, -11,   7,  26,  24,  35,  -8, -20,
+    -36, -26, -12,  -1,   9,  -7,   6, -23,
+    -45, -25, -16, -17,   3,   0,  -5, -33,
+    -44, -16, -20,  -9,  -1,  11,  -6, -71,
+    -19, -13,   1,  17,  16,   7, -37, -26,
+]
+PST_ROOK_EG = [
+     13,  10,  18,  15,  12,  12,   8,   5,
+     11,  13,  13,  11,  -3,   3,   8,   3,
+      7,   7,   7,   5,   4,  -3,  -5,  -3,
+      4,   3,  13,   1,   2,   1,  -1,   2,
+      3,   5,   8,   4,  -5,  -6,  -8, -11,
+     -4,   0,  -5,  -1,  -7, -12,  -8, -16,
+     -6,  -6,   0,   2,  -9,  -9, -11,  -3,
+     -9,   2,   3,  -1,  -5, -13,   4, -20,
+]
+
+PST_QUEEN_MG = [
+    -28,   0,  29,  12,  59,  44,  43,  45,
+    -24, -39,  -5,   1, -16,  57,  28,  54,
+    -13, -17,   7,   8,  29,  56,  47,  57,
+    -27, -27, -16, -16,  -1,  17,  -2,   1,
+     -9, -26,  -9, -10,  -2,  -4,   3,  -3,
+    -14,   2, -11,  -2,  -5,   2,  14,   5,
+    -35,  -8,  11,   2,   8,  15,  -3,   1,
+     -1, -18,  -9,  10, -15, -25, -31, -50,
+]
+PST_QUEEN_EG = [
+     -9,  22,  22,  27,  27,  19,  10,  20,
+    -17,  20,  32,  41,  58,  25,  30,   0,
+    -20,   6,   9,  49,  47,  35,  19,   9,
+      3,  22,  24,  45,  57,  40,  57,  36,
+    -18,  28,  19,  47,  31,  34,  12,  11,
+    -16, -27,  15,   6,   9,  17,  10,   5,
+    -22, -23, -30, -16, -16, -23, -36, -32,
+    -33, -28, -22, -43,  -5, -32, -20, -41,
+]
+
+PST_KING_MG = [
+    -65,  23,  16, -15, -56, -34,   2,  13,
+     29,  -1, -20,  -7,  -8,  -4, -38, -29,
+     -9,  24,   2, -16, -20,   6,  22, -22,
+    -17, -20, -12, -27, -30, -25, -14, -36,
+    -49, -1, -27, -39, -46, -44, -33, -51,
+    -14, -14, -22, -46, -44, -30, -15, -27,
+      1,   7,  -8, -64, -43, -16,   9,   8,
+    -15,  36,  12, -54,   8, -28,  24,  14,
+]
+PST_KING_EG = [
+    -74, -35, -18, -18, -11,  15,   4, -17,
+    -12,  17,  14,  17,  17,  38,  23,  11,
+     10,  17,  23,  15,  20,  45,  44,  13,
+     -8,  22,  24,  27,  26,  33,  26,   3,
+    -18,  -4,  21,  24,  27,  23,   9, -11,
+    -19,  -3,  11,  21,  23,  16,   7,  -9,
+    -27, -11,   4,  13,  14,   4,  -5, -17,
+    -53, -34, -21, -11, -28, -14, -24, -43,
+]
+
 _PST_MAP = {
     chess.PAWN:   PST_PAWN,
     chess.KNIGHT: PST_KNIGHT,
@@ -112,13 +238,41 @@ def _manhattan(sq1: int, sq2: int) -> int:
 
 def _endgame_factor(board: chess.Board) -> float:
     phase = 0
+    phase += len(board.pieces(chess.KNIGHT, chess.WHITE)) * 1
+    phase += len(board.pieces(chess.KNIGHT, chess.BLACK)) * 1
+    phase += len(board.pieces(chess.BISHOP, chess.WHITE)) * 1
+    phase += len(board.pieces(chess.BISHOP, chess.BLACK)) * 1
+    phase += len(board.pieces(chess.ROOK, chess.WHITE)) * 2
+    phase += len(board.pieces(chess.ROOK, chess.BLACK)) * 2
+    phase += len(board.pieces(chess.QUEEN, chess.WHITE)) * 4
+    phase += len(board.pieces(chess.QUEEN, chess.BLACK)) * 4
     for c in (chess.WHITE, chess.BLACK):
         phase += len(board.pieces(chess.QUEEN,  c)) * 4
         phase += len(board.pieces(chess.ROOK,   c)) * 2
         phase += len(board.pieces(chess.BISHOP, c))
         phase += len(board.pieces(chess.KNIGHT, c))
+
     return 1.0 - min(phase, 24) / 24.0
 
+def _get_pst(piece_type, eg):
+    mg_weight = 1.0 - eg
+    if piece_type == chess.PAWN: return [(PST_PAWN_MG[i]*mg_weight + PST_PAWN_EG[i]*eg) for i in range(64)]
+    elif piece_type == chess.KNIGHT: return [(PST_KNIGHT_MG[i]*mg_weight + PST_KNIGHT_EG[i]*eg) for i in range(64)]
+    elif piece_type == chess.BISHOP: return [(PST_BISHOP_MG[i]*mg_weight + PST_BISHOP_EG[i]*eg) for i in range(64)]
+    elif piece_type == chess.ROOK: return [(PST_ROOK_MG[i]*mg_weight + PST_ROOK_EG[i]*eg) for i in range(64)]
+    elif piece_type == chess.QUEEN: return [(PST_QUEEN_MG[i]*mg_weight + PST_QUEEN_EG[i]*eg) for i in range(64)]
+    else: return [(PST_KING_MG[i]*mg_weight + PST_KING_EG[i]*eg) for i in range(64)]
+
+def _evaluate_draw(board: chess.Board, current_eval: float) -> float:
+    if not (board.is_stalemate() or board.is_repetition(2) or board.is_fifty_moves() or board.is_insufficient_material()):
+        return 0.0
+    
+    perspective_eval = current_eval if _ROOT_COLOR == chess.WHITE else -current_eval
+    if perspective_eval > 50:
+        return -99000 if _ROOT_COLOR == chess.WHITE else 99000
+    elif perspective_eval < -50:
+        return 99000 if _ROOT_COLOR == chess.WHITE else -99000
+    return 0.0
 
 def _material_and_pst(board: chess.Board, eg: float) -> float:
     score = 0.0
@@ -712,27 +866,89 @@ def _fifty_move_pressure(board: chess.Board) -> float:
     return -pressure if _ROOT_COLOR == chess.WHITE else pressure
 
 
+import chess
+from math import log as _log
+
+def _anti_ai_complexity(board: chess.Board, eg: float) -> float:
+    score = 0.0
+    
+
+    if len(board.pieces(chess.BISHOP, chess.WHITE)) >= 2: score += 40
+    if len(board.pieces(chess.BISHOP, chess.BLACK)) >= 2: score -= 40
+    
+    if eg < 0.5:
+        w_pawns = len(board.pieces(chess.PAWN, chess.WHITE))
+        b_pawns = len(board.pieces(chess.PAWN, chess.BLACK))
+        score += (w_pawns - b_pawns) * 10
+        
+        w_pieces = len(board.piece_map())
+        if w_pieces > 20: 
+            score += 20 if board.turn == chess.WHITE else -20
+            
+    return score
+
+def _fork_and_threat_evaluation(board: chess.Board) -> float:
+    score = 0.0
+    VALUABLE = {chess.KNIGHT: 1, chess.BISHOP: 1, chess.ROOK: 2, chess.QUEEN: 3, chess.KING: 4}
+
+    for color in (chess.WHITE, chess.BLACK):
+        sign = 1 if color == chess.WHITE else -1
+        enemy = not color
+        
+        for sq, piece in board.piece_map().items():
+            if piece.color != color:
+                continue
+                
+            attacked_squares = board.attacks(sq)
+            high_value_targets = 0
+            
+            for target_sq in attacked_squares:
+                target_piece = board.piece_at(target_sq)
+                if target_piece and target_piece.color == enemy:
+                    if target_piece.piece_type in VALUABLE:
+                        if PIECE_VALUES[piece.piece_type] < PIECE_VALUES[target_piece.piece_type]:
+                            high_value_targets += 1
+                        elif not board.is_attacked_by(color, target_sq):
+                            high_value_targets += 1
+                            
+            if high_value_targets >= 2:
+                score += sign * 150 
+                
+    return score
+
 def evaluate(board: chess.Board) -> float:
     if board.is_checkmate():
-        return -99_999 if board.turn == chess.WHITE else 99_999
-
+        return -99999 if board.turn == chess.WHITE else 99999
     if (board.is_stalemate()
             or board.is_insufficient_material()
             or board.is_repetition(3)
             or board.is_fifty_moves()):
-        return _contempt_score()
-
+        return -60 if _ROOT_COLOR == chess.WHITE else 60
     if board.is_repetition(2):
-        return _contempt_score() * 3.0
+        return (-60 if _ROOT_COLOR == chess.WHITE else 60) * 3.0
 
-    eg  = _endgame_factor(board)
-    mat = _material_and_pst(board, eg)
+    eg = _endgame_factor(board)
+    score = 0.0
 
-    score  = mat
+    score += _material_and_pst(board, eg)
+
     score += _pawn_structure(board)
     score += _pawn_chain_bonus(board)
-    score += _mobility(board)
+
+    w = b = 0
+    for sq in chess.SQUARES:
+        piece = board.piece_at(sq)
+        if piece:
+            n = len(board.attacks(sq))
+            if piece.color == chess.WHITE:
+                w += n
+            else:
+                b += n
+    score += 0.1 * (w - b)
+
     score += _king_safety(board, eg)
+    score += _king_attack_zone(board, eg)  
+
     score += _pawn_storm(board, eg)
     score += _castling_bonus(board)
     score += _bishop_pair(board)
@@ -740,10 +956,41 @@ def evaluate(board: chess.Board) -> float:
     score += _knight_outposts(board)
     score += _rook_on_seventh(board)
     score += _connected_rooks(board)
+
     score += _hanging_pieces(board)
+
     score += _fork_evaluation(board)
-    score += _king_proximity_mop_up(board, eg, mat)
-    score += _king_attack_zone(board, eg)
+
+    for sq in chess.SQUARES:
+        piece = board.piece_at(sq)
+        if piece is None or piece.piece_type != chess.KNIGHT:
+            continue
+        sign = 1 if piece.color == chess.WHITE else -1
+        enemy = not piece.color
+
+        for move in board.legal_moves:
+            if move.from_square != sq:
+                continue
+
+            tsq = move.to_square
+            occupant = board.piece_at(tsq)
+            if occupant is not None and occupant.color == piece.color:
+                continue
+
+            future_vals = []
+            for atsq in chess.SquareSet(chess.BB_KNIGHT_ATTACKS[tsq]):
+                if atsq == sq:
+                    continue
+                tp = board.piece_at(atsq)
+                if tp and tp.color == enemy and tp.piece_type in {chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING}:
+                    future_vals.append(PIECE_VALUES[tp.piece_type])
+
+            if len(future_vals) >= 2:
+                vals = sorted(future_vals, reverse=True)[:2]
+                weight = 0.06 if occupant is None else 0.03
+                score += sign * (vals[0] + vals[1]) * weight 
+
+    score += _king_proximity_mop_up(board, eg, _material_and_pst(board, eg))
     score += _space_advantage(board, eg)
     score += _trapped_pieces(board)
     score += _queen_tropism(board, eg)
@@ -751,11 +998,36 @@ def evaluate(board: chess.Board) -> float:
     score += _rule_of_square(board, eg)
     score += _king_activity_endgame(board, eg)
     score += _king_opposition(board, eg)
-    score += 15 if board.turn == chess.WHITE else -15
 
+    material_score = 0.0
+    for pt in PIECE_VALUES:
+        pst = _get_pst(pt, eg)
+        for sq in board.pieces(pt, chess.WHITE):
+            material_score += PIECE_VALUES[pt] + pst[chess.square_mirror(sq)]
+        for sq in board.pieces(pt, chess.BLACK):
+            material_score -= PIECE_VALUES[pt] + pst[sq]
+                
+    score += material_score
+        
+    draw_score = _evaluate_draw(board, score)
+    if draw_score != 0.0:
+        return draw_score
+     
+    w_attacks = 0
+    b_attacks = 0
+    for sq, piece in board.piece_map().items():
+        if piece.color == chess.WHITE:
+            w_attacks += len(board.attacks(sq))
+        else:
+            b_attacks += len(board.attacks(sq))
+    score += 0.05 * (w_attacks - b_attacks)
+    
+    score += _fork_and_threat_evaluation(board)
+    score += _anti_ai_complexity(board, eg)
+
+    score += (15 if board.turn == chess.WHITE else -15)
     if board.is_check():
-        score += 20 if board.turn == chess.BLACK else -20
-
+        score += (20 if board.turn == chess.BLACK else -20)
     return score
 
 
@@ -768,7 +1040,7 @@ TT_EXACT = 0
 TT_LOWER = 1
 TT_UPPER = 2
 
-TARGET_DEPTH = 30
+TARGET_DEPTH = 10
 
 _FUTILITY_MARGINS = {1: 120, 2: 300, 3: 500}
 _LMP_CUTOFFS      = {1: 8, 2: 12, 3: 20}
